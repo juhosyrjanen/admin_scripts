53382a09bf620f15790fdc3932a26ef7c0768005
Juho Syrjänen <syrjanen.juho@gmail.com>
2019-03-29 15:54:15 +0200

    removed excessive variables and cleaned up a bit

--- dump.sh
+++ dump.sh
@@ -6,7 +6,6 @@
     export DB_BACKUP=/home/mysql_backup
     export DB_USER=backup
     export DB_PASSWD=testi123
-    export DATE=`date +”%d%b”`
     export MYSQL=mysql
     export MYSQLDUMP=mysqldump
 
@@ -21,7 +20,7 @@
     mkdir $DB_BACKUP/01
 
     #tehdään uusi dumppi // kannan kannattaa olla melko pieni tätä varten. Ajaa kaikki databaset erillisiin zippeihin
-    cd $DB_BACKUP/ && cd $DB_BACKUP/01
+    cd $DB_BACKUP/01
     $MYSQL -u $DB_USER --password=$DB_PASSWD -Bse "show databases" |while read m; \
     do $MYSQLDUMP --single-transaction -u $DB_USER --password=$DB_PASSWD `echo $m` > `echo $m`.sql;done
     #voi myös käyttää zippiä, mutta tämä ilmeisesti nopeampi
