f5915a2e93729fb386f0f9a7036395aa8c3c4801
Juho Syrjänen <syrjanen.juho@gmail.com>
2020-12-14 14:32:21 +0200

    Update sftp-userdel.sh
    added logging

--- sftp-userdel.sh
+++ sftp-userdel.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #juho@syrjanen.org
 
+logfile="/var/log/sftp.log"
+timestamp=`date "+%Y-%m-%d %H:%M:%S"`
+
 #Root check
 if ! [ $(id -u) = 0 ]; then
    echo "Please run as sudo."
@@ -14,29 +17,52 @@ reset=`tput sgr0`
 
 ## Set username variable
 echo -e "--- ${green}Valtti ${reset}SFTP service ---"
-echo -e "This script will delete SFTP user and all its files."
+echo -e "This script will create an SFTP user for new SFTP client."
 echo
-echo -e "Enter new SFTP user's username and press [ENTER] or to exit without changes press CTRL+C"
+echo -e "Enter new SFTP client username and press [ENTER] or to exit without changes press CTRL+C"
 read username
 echo
 
+#check if user already exists
 if id "$username" &>/dev/null; then
-    echo 'User '$username' found.'
-else
-    echo 'User '$username' not found! Exiting..'
+    echo 'User '$username' already exists.'
     exit
 fi
 
-echo -e 
+# Generating password for new user
+echo -e "Generating random password for user "$username".."
+pw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+sleep 2
+echo -e "...."
+
+#Add user with username variable
+useradd $username
+#Set password for user
+echo $username:$pw | chpasswd
+#Set SFTP-group for user
+usermod -aG sftpuser $username
+
+#Create necessary folders  for chroot jail
+echo
+echo -e "Creating SFTP folders for " $username".."
+mkdir /sftp/$username
 
 while true; do
-    read -p "Delete user "$username" and all its files? ${red}THIS IS A PERMANENT ACTION! ${reset}[yes/no]? " yn
+    read -p "Create read-only user? [Y/N] ?" yn
     case $yn in
-        [Yy]* ) userdel $username && rm -rf /sftp/$username ; break;;
-        [Nn]* ) exit;;
+        [Nn]* ) mkdir /sftp/$username/uploads && cd /sftp/$username && chown $username:$username uploads; echo "SFTP user "$username "has been created with the following password:" $pw && sleep 2 ; exit;;
+        [Yy]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-echo -e 
-echo "${green}User "$username" and its files have been deleted. ${reset}"
+echo "SFTP read-only user "$username "has been created with the following password:" $pw
+echo
+
+#logging action
+touch $logfile
+id=$(whoami) 
+echo "$timestamp useradd script run by $id." >> $logfile
+echo "Added user $username." >> $logfile
+
+sleep 2
