fc49300e0460dc5c1f8b3b6cd19e0ccb5ec0939b
Juho Syrjänen <syrjanen.juho@gmail.com>
2020-12-14 13:46:56 +0200

    Update sftp-useradd.sh

--- sftp-useradd.sh
+++ sftp-useradd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#juho@syrjanen.org
 
 #Root check
 if ! [ $(id -u) = 0 ]; then
@@ -6,19 +7,24 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-##set variables prior to running
-
-sftpgroup=sftpuser
-#path without /
-mountpoint=sftp
+#text color variables
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 ## Set username variable
-echo -e "--- **** SFTP service ---"
+echo -e "--- ${green}Valtti ${reset}SFTP service ---"
 echo -e "This script will create an SFTP user for new SFTP client."
 echo
 echo -e "Enter new SFTP client username and press [ENTER] or to exit without changes press CTRL+C"
 read username
 echo
+
+#check if user already exists
+if id "$username" &>/dev/null; then
+    echo 'User '$username' already exists.'
+    exit
+fi
 
 # Generating password for new user
 echo -e "Generating random password for user "$username".."
@@ -31,20 +37,22 @@ useradd $username
 #Set password for user
 echo $username:$pw | chpasswd
 #Set SFTP-group for user
-usermod -aG $sftpgroup $username
+usermod -aG sftpuser $username
 
 #Create necessary folders  for chroot jail
 echo
 echo -e "Creating SFTP folders for " $username".."
-mkdir /$mountpoint/$username
-mkdir /$mountpoint/$username/uploads
+mkdir /sftp/$username
 
-#Set correct permissions for uploads-folder, sftp chroot jail forbids write on chroot, user must write in uploads-folder.
-chown $username:$username /$mountpoint/$username/uploads
+while true; do
+    read -p "Create read-only user? [Y/N] ?" yn
+    case $yn in
+        [Nn]* ) mkdir /sftp/$username/uploads && cd /sftp/$username && chown $username:$username uploads; echo "SFTP user "$username "has been created with the following password:" $pw && sleep 2 ; exit;;
+        [Yy]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
+echo "SFTP read-only user "$username "has been created with the following password:" $pw
 echo
-echo -e " - - -"
-echo "SFTP user "$username "has been created with the following password:" $pw
-echo -e " - - -"
 sleep 2
-exit
