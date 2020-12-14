0c9bf7d365c8972d87514de415a1515c2d416e33
Juho Syrjänen <juhosyrjanen@juho-mb.lan>
2020-12-14 14:00:48 +0200

    how to add colors to echo promts.

---
+++ terminal_colors.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6` 
+reset=`tput sgr0`
+
+echo "${green} Set ${blue} terminal ${magenta} colors ${red} like ${cyan} this. ${reset}"
