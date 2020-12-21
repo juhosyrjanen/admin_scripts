699e95af5710c148d0f2daad6f70b318e88e7b48
Juho Syrjänen <syrjanen.juho@gmail.com>
2020-12-21 09:11:46 +0200

    distribution check & firewall conf added.

--- sftp-configure.sh
+++ sftp-configure.sh
@@ -1,37 +1,88 @@
-#!/bin/bash
+##!/bin/bash
 #juho@syrjanen.org
 
-logfile="/var/log/sftp.log"
+logfile="/var/log/valtti-sftp.log"
 timestamp=`date "+%Y-%m-%d %H:%M:%S"`
 sshconf=/etc/ssh/sshd_config
 conf=sftpuser
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-
-
 #Root check
 if ! [ $(id -u) = 0 ]; then
    echo "Please run as sudo."
    exit 1
 fi
+#Root checks ends
 
-echo "This script will configure server to use SFTP service. Use for new servers only!"
+#notify
+echo "This script will configure server to use Valtti SFTP service. Use for new servers only!"
 echo -e
 echo "Checking if configuration is already in place..."
 
-#grepping for existing configuration. simple check, needs work.
+#Conf check starts
 if grep -q "$conf" "$sshconf"; then
   echo "Configuration is in place or similar exists. Exiting."
   exit 1
 else 
   echo "No existing configuration detected. Resuming.."
 fi
+#Conf check ends
 
+#user confirmation
+while true; do
+    read -p "Configure this server to use Valtti SFTP service? [yes/no]? " yn
+    case $yn in
+        [Yy]* ) echo "Starting config.."; break;;
+        [Nn]* ) echo "Exiting.."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#Backup starts
+sleep 1
 echo -e "Creating backup of sshd_config."
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sleep 1
 echo "To revert configuration run following command manually: cp /etc/ssh/sshd_confif.bak /etc/ssh/sshd_config"
 echo -e
+sleep 1
+#Backup ends
+
+#OS check starts
+distrocheck=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+
+if [[ $distrocheck == *"centos"* ]]; then
+  echo "Running CentOS.."
+  os=rhel
+elif [[ $distrocheck == *"ubuntu"* ]]; then
+  echo "Running Ubuntu.."  
+  os=ubuntu
+elif [[ $distrocheck == *"rhel"* ]]; then
+  echo "Running RHEL.."  
+  os=rhel
+elif [[ $distrocheck == *"debian"* ]]; then
+  echo "Running Debian.."  
+  os=deb
+fi
+#OS check ends
+
+echo -e 
+echo "Running firewall configuration.."
+#Firewall configuration starts
+#assuming ubuntu runs ufw, rhel/centos firewalld, debian iptables..
+if [[ $os == *"rhel"* ]] && [ -e /bin/firewall-cmd ]; then
+  firewall-cmd --permanent --add-service=ssh
+  systemctl enable firewalld
+  systemctl restart firewalld
+elif [[ $os == *"ubuntu"* ]]; then
+  ufw allow ssh
+  echo "y" | sudo ufw enable
+elif [[ $distrocheck == *"debian"* ]] && [ -e /etc/sysconfig/iptables ]; then
+  	iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
+	/sbin/service iptables save
+fi
+#Firewall configuration ends
 
 #Remove default sftp conf
 sed -i '/Subsystem/d' /etc/ssh/sshd_config
@@ -47,9 +98,20 @@ Subsystem   sftp    internal-sftp
 EOF
 
 #Create sftp directories and set permissions
-mkdir /sftp
-chown root:root /sftp
-chmod 755 /sftp
+
+echo "Checking if /sftp folder already exists.."
+if [ -d "/sftp" ] 
+then
+    echo "/sftp exists. Setting permissions.." 
+    chown root:root /sftp
+    chmod 755 /sftp
+
+else
+    echo "/sftp does not exist, creating and setting permissions.."
+    mkdir /sftp
+    chown root:root /sftp
+    chmod 755 /sftp
+fi
 
 #Create SFTP group
 groupadd sftpuser
@@ -68,4 +130,3 @@ echo "======="
 touch $logfile
 id=$(whoami) 
 echo "$timestamp SFTP configuration script run by $id." >> $logfile
-
